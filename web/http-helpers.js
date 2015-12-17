8f4f52039403ab42d97d9585a718979dd968fe7b
Ben Grinberg <bengrin25@gmail.com>
2015-12-16 20:16:20 -0500

    passed get GET tests

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,8 +14,46 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+ 
+ //read file siteassets + asset, function
+  // if err
+    // read archivedSites
+      //if  error
+        //send 404
+    // if no erro 
+        //send response (res data)
+ //if no error
+ // send response( res data)
+
+ var assetPath = archive.paths.siteAssets + '/' + asset;
+ var archivedPath = archive.paths.archivedSites + '/' + asset;
+ //console.log('test', archivedPath)
+
+ 
+  fs.readFile(assetPath, "utf8", function (err, data) {
+    if (err) { 
+      fs.readFile(archivedPath, "utf8", function (err, data) {
+        if (err){
+          console.log("file not found")
+          callback('err');
+        } else {
+          callback(data);
+        }
+      })
+    } else {
+      callback(data);
+    }
+  })
+
+ 
+
+
 };
 
-
+exports.sendResponse = function(response, data, statusCode){
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(data);
+};
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,59 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelper = require('./http-helpers');
+var url = require('url')
+
+var actions = {
+
+  'GET' : function (req, res) {
+    var pathname = url.parse(req.url).pathname;
+    if (pathname === '/'){
+      pathname = 'index.html';
+    }
+    httpHelper.serveAssets(req, pathname, function(data){
+      //httpHelper.sendResponse(req, data)
+      if (data === 'err') {
+      res.writeHead(404, httpHelper.headers);
+      res.end();
+      } else {
+      res.writeHead(200, httpHelper.headers);
+      res.end(data);
+      }
+    });
+  
+  }
+}
+
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+  if (action){
+    action(req, res);
+  } else {
+    httpHelper.sendResponse(res, "Not found", 404)
+  }
+
+
+  // if (req.url === "/") {
+  //   httpHelper.serveAssets(res, "index.html", function (data) {
+  //     res.writeHead(201, httpHelper.headers);
+  //     res.write(data);
+  //     res.end();
+  //   })
+  
+  // } else if (req.url) {
+  //   console.log("url",req.url)
+    
+  //   httpHelper.serveAssets(res, req.url, function (data) {
+  //     //console.log(data)
+  //     res.writeHead(201, httpHelper.headers);
+  //     res.write(data);
+  //     res.end();
+  //   })
+  // }
+
+
+
+  //res.end(archive.paths.list);
 };
